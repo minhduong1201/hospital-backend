51351060eb69b5f3128ff818c89f497ddf2a9f3b
minhduong1201 <minhduongahlg@gmail.com>
2023-07-25 21:11:34 +0700

    update

--- models/Customer.js
+++ models/Customer.js
@@ -3,9 +3,9 @@ const mongoose=require("mongoose");
 const CustomerSchema=new mongoose.Schema(
     {
     name: {type: String, required: true},
-    username:{type:String,required:true,unique:true},
+    username:{type:String,required:true, unique:true},
     password:{type:String,required:true},
-    phone: {type: String, unique: true},
+    phone: {type: String},
     address: {type: String},
     age: {type: Number},
     hospitalId: {type: String},
